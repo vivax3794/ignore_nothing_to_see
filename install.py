88330e977528c9249df494c04361cfcbbfc8ff08
vivax3794 <github@vivax3794.com>
2024-04-22 01:08:50 +0200

    ignore

--- install.py
+++ install.py
@@ -1,8 +1,10 @@
-import os
+# import os
+#
+# open("hacked.txt", "w").write("""
+# Pro tip: Dont open random links from strangers.
+#
+# - Viv
+# """)
+# os.system("notepad hacked.txt")
 
-open("hacked.txt", "w").write("""
-Pro tip: Dont open random links from strangers.
-
-- Viv
-""")
-os.system("notepad hacked.txt")
+print("AJAJSDJIOALSJDIOLAJDSIO")
